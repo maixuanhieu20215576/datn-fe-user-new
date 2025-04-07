89538462ecefa809b529adb554569b531c24c4b9
maixuanhieu135 <111688465+maixuanhieu135@users.noreply.github.com>
2025-04-07 10:03:38 +0700

    fix bug price class

--- src/pages/Class/ClassDetail.tsx
+++ src/pages/Class/ClassDetail.tsx
@@ -83,7 +83,7 @@ export default function ClassDetail() {
         try {
             setIsRedirecting(true);
             const response = await axios.post(`${import.meta.env.VITE_API_URL}/payment`, {
-                price: classData.priceType === constants.priceType.byDay ? classData.price : classData.price * classData.schedule.length,
+                price: classData.priceType === constants.priceType.byDay ? classData.price * classData.schedule.length : classData.price,
                 userId,
                 classId: id,
             });
